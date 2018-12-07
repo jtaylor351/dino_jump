616ff4d02560fff1d8ec34325114835a83503792
Jordan <jtaylor351@gatech.edu>
2018-12-07 15:03:33 -0500

    Update myLib.c

--- myLib.c
+++ myLib.c
@@ -1,11 +1,8 @@
 #include "myLib.h"
 #include "pics.h"
 
-// extern unsigned short birdList[] = {dinoRun1, dinoRun2};
-
 unsigned short *videoBuffer = (unsigned short *)0x6000000;
 
-// const unsigned short *cacti= {cactus1, cactus2};
 void drawSplash(int row, int col, int height, int width, const unsigned short *img);
 
 void setPixel(int row, int col, unsigned short color)
@@ -26,25 +23,9 @@ void drawRect(int row, int col, int height, int width, volatile unsigned short c
 
 void drawSprite(SPRITE *sprite)
 {
-	// for (int r = 0; r < sprite->height; r++)
-	// {
-    //     for (int c = 0; c < sprite->width; c++)
-	// 	{
-	// 		if (sprite->img[r * sprite->width + c] == 0)
-	// 		{
-	// 			setPixel(sprite->row + r, c + sprite->col, LTGRAY);
-	// 		}
-	// 		else
-	// 		{
-	// 			unsigned short image_pixel = sprite->img[(r * sprite->width) + c];
-	// 			setPixel(sprite->row + r, c + sprite->col, image_pixel);
-	// 		}
-    //     }
-    // }
 	drawSplash(sprite->row, sprite->col, sprite->height, sprite->width, sprite->img);
 }
 
-// void drawPartialCactus()
 
 void drawSplash(int row, int col, int height, int width, const unsigned short *img)
 {
@@ -120,36 +101,10 @@ int testCollision(SPRITE *s1, SPRITE *s2)
 		}
 	}
 	return 0;
-	// else
-	// {
-	// 	// if we get here we know there is overlap
-	// 	if ((s1->row + s1->height) >= s2->row)
-	// 	{
-	// 		if (s2->col < s1->col) {
-	// 			return 0;
-	// 		}
-	// 		return 1;
-	// 	}
-	// 	// if () {
-	// 	//
-	// 	// }
-	// }
-	//
-	// return 0;
-
 }
 
 void eraseSprite(SPRITE *sprite, u32 *bg)
 {
-	// for(int r=0; r<sprite->height; r++)
-	// {
-	// 	REG_DMA3SAD = (u32)(img + r * width); // This is always an address!
-	// 	// printf(REG_DMA3SAD)
-	// 	REG_DMA3DAD = (u32)&videoBuffer[OFFSET(row+r, col, 240)]; // This is always an address!
-	// 	//REG_DMA3DAD = (u32)(videoBuffer + OFFSET(row+r, col, 240) ); // This is always an address!
-	// 	REG_DMA3CNT = (width) | DMA_ON | DMA_DESTINATION_INCREMENT | DMA_SOURCE_INCREMENT;
-	//
-	// }
 	for(int r=0; r<sprite->height; r++)
 	{
 		REG_DMA3SAD = (u32)bg; // This is always an address!
@@ -157,22 +112,6 @@ void eraseSprite(SPRITE *sprite, u32 *bg)
 		REG_DMA3CNT = (sprite->width) | DMA_ON | DMA_DESTINATION_INCREMENT | DMA_SOURCE_FIXED;
 	}
 }
-
-
-/*
-void reset()
-{
-	cact1.col = 240 - CACTUS1_WIDTH;
-	cact2.col = 240 - CACTUS2_WIDTH;
-	cact3.col = 240 - CACTUS3_WIDTH;
-	cact4.col = 240 - CACTUS4_WIDTH;
-	bird.col = 240 - BIRD1_WIDTH;
-}
-
-*/
-
-// void drawScene(GAME_STATE *state)
-// {
 // 	if (state->cactusOnScreen)
 // 	{
 // 		drawSprite(40, 40)
